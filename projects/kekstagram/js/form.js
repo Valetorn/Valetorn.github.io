4c08193df71d18fcc60d877f8c1d74f8ae98fac5
Vladislav Durin <rock_n_vladok@mail.ru>
2017-04-16 20:23:54 +0400

    Changed initialize-filters.js

--- projects/kekstagram/js/form.js
+++ projects/kekstagram/js/form.js
@@ -11,6 +11,7 @@ var uploadFilter = document.getElementById('upload-filter');
 var oldFilter = null;
 var filterDefault = document.getElementById('upload-filter-none').value;
 var scaleDefault = 100;
+var uploadFilterLevel = document.querySelector('.upload-filter-level');
 
 var changeAriaAttribute = function (isOpen) {
   uploadOverlay.setAttribute('aria-pressed', !isOpen);
@@ -27,21 +28,36 @@ var openUploadOverlay = function () {
   uploadOverlay.classList.remove('invisible');
   document.addEventListener('keydown', uploadOverlayKeydownHandler);
   changeAriaAttribute(true);
+  applyFilter(filterDefault);
+  adjustScale(scaleDefault);
 };
 var closeUploadOverlay = function () {
   uploadOverlay.classList.add('invisible');
   uploadSelectImage.classList.remove('invisible');
   document.removeEventListener('keydown', uploadOverlayKeydownHandler);
   changeAriaAttribute(false);
-  applyFilter(filterDefault);
-  adjustScale(scaleDefault);
 };
 var applyFilter = function (newFilter) {
   if (oldFilter) {
     filterImagePreview.classList.remove('filter-' + oldFilter);
+    filterImagePreview.style.filter = '';
   }
   filterImagePreview.classList.add('filter-' + newFilter);
   oldFilter = newFilter;
+  if (filterImagePreview.classList.contains('filter-none') || filterImagePreview.classList.contains('filter-phobos') || filterImagePreview.classList.contains('filter-heat')) {
+    uploadFilterLevel.classList.add('invisible');
+  } else {
+    uploadFilterLevel.classList.remove('invisible');
+  }
+};
+window.applyFilterRange = function (filterRangeValue) {
+  if (filterImagePreview.classList.contains('filter-sepia')) {
+    filterImagePreview.style.filter = 'sepia(' + filterRangeValue + ')';
+  } else if (filterImagePreview.classList.contains('filter-chrome')) {
+    filterImagePreview.style.filter = 'grayscale(' + filterRangeValue + ')';
+  } else if (filterImagePreview.classList.contains('filter-marvin')) {
+    filterImagePreview.style.filter = 'invert(' + filterRangeValue * 100 + '%' + ')';
+  }
 };
 var adjustScale = function (scale) {
   filterImagePreview.style.transform = 'scale(' + parseInt(scale, 10) / 100 + ')';
